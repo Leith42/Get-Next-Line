349ea356a91a97951e0c44e33bdee807e48846a2
Amine <aazri@student.42.fr>
2016-12-19 17:28:16 +0100

    neufin jeun sneuw

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,36 @@
 /*   By: aazri <aazri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/16 15:39:39 by aazri             #+#    #+#             */
-/*   Updated: 2016/12/16 16:57:29 by aazri            ###   ########.fr       */
+/*   Updated: 2016/12/19 17:10:09 by aazri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	get_next_line(const int fd, char **line)
-{
+char**	get_next_line(const int fd, char **line);
 
+int main(int argc, char const *argv[])
+{
+	char **line = NULL;
+	if(argc != 2)
+		exit(0);
+	get_next_line(open(argv[1], O_RDONLY), line);
+	//printf("%s", *line);
+	return 0;
+}
+
+char	**get_next_line(const int fd, char **line)
+{
+	size_t i = 0;
+	size_t buff_tmp = BUFF_SIZE;
+	char buff[BUFF_SIZE];
+
+	while(read(fd, &buff[i], 1) && i < BUFF_SIZE && buff[i] != '\n')
+	{
+		buff_tmp--;
+		i++;
+	}
+	buff[i] = 0;
+	printf("%s", buff);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,20 @@
 /*   By: aazri <aazri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/16 15:39:53 by aazri             #+#    #+#             */
-/*   Updated: 2016/12/16 16:57:32 by aazri            ###   ########.fr       */
+/*   Updated: 2016/12/19 16:03:07 by aazri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_h
+# define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 32
 
-int	get_next_line(const int fd, char **line);
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+//int	get_next_line(const int fd, char **line);
 
 #endif
